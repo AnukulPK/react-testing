563139f56683968c0d343a3c41468dd465bc81f5
unknown <anukulpk2@gmail.com>
2022-12-21 11:11:08 +0530

    app rendered

--- src/componets/App.js
+++ src/componets/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return <div>I am the app component</div>;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import App from "./componets/App";
+import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
