4e5cf2cef605263d484d1c0df78dfe9d95c38c82
unknown <anukulpk2@gmail.com>
2023-02-22 14:48:59 +0530

    comment box created

--- src/componets/CommentBox/CommentBox.jsx
+++ src/componets/CommentBox/CommentBox.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const CommentBox = () => {
-  return <div>Comment Box</div>;
+  return (
+    <form>
+      <h4>Add a Comment</h4>
+      <textarea />
+      <div>
+        <button>Submit Comment</button>
+      </div>
+    </form>
+  );
 };
 
 export default CommentBox;
